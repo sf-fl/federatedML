85158de6574b25563122580cb691dac17cc6f242
windwithmind <35332465+windwithmind@users.noreply.github.com>
2020-12-09 15:54:41 +0800

    简化预处理
    简化预处理，提前给数据集做完

--- test/client/federation/feature_engineering.py
+++ test/client/federation/feature_engineering.py
@@ -1,12 +1,14 @@
 import copy
 
 def feating(x_raw,y_raw):
-    args = x_raw.describe()
-    x = copy.deepcopy(x_raw)
-    for i in range(x_raw.shape[1]):
-        xi = x_raw.iloc[:,i]
-        max = xi.max()
-        min = xi.min()
-        x.iloc[:,i] = x.iloc[:,i].apply(lambda xx: int((xx - min) / (max - min) * 100 // 1))
-    y = y_raw.apply(lambda yy : yy*2-1)
+    # args = x_raw.describe()
+    # x = copy.deepcopy(x_raw)
+    # for i in range(x_raw.shape[1]):
+    #     xi = x_raw.iloc[:,i]
+    #     max = xi.max()
+    #     min = xi.min()
+    #     x.iloc[:,i] = x.iloc[:,i].apply(lambda xx: int((xx - min) / (max - min) // 1))
+    # y = y_raw.apply(lambda yy : yy*2-1)
+    x = x_raw
+    y = y_raw
     return x,y
